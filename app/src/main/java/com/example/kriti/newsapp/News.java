9bad8441c7e9e1a6a09cc8cf42cf3dced34a8d13
Kriti Garg <kritigarg1998@gmail.com>
2018-04-08 00:27:42 +0530

    Add News.java

---
+++ app/src/main/java/com/example/kriti/newsapp/News.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.kriti.newsapp;
+
+/**
+ * An {@link News} object contains information related to a single book.
+ */
+public class News {
+
+    private String mHeadline;
+    private String mAuthor;
+    private String mDate;
+    private String mGenre;
+    private String mUrl;
+
+
+    /**
+     * Constructs a new {@link News} object.
+     *
+     * @param headline is the headline of the news
+     * @param author is the author of the news
+     * @param date is the date of the news
+     * @param genre is the genre of the news
+     * @param url is the website URL of the news.
+     */
+    public News(String headline, String author, String date, String genre, String url) {
+        mHeadline = headline;
+        mAuthor = author;
+        mDate = date;
+        mGenre = genre;
+        mUrl = url;
+    }
+
+    /**
+     * Returns the headline of the news.
+     */
+    public String getHeadline() {
+        return mHeadline;
+    }
+
+    /**
+     * Returns the author's name of the news.
+     */
+    public String getAuthor() {
+        return mAuthor;
+    }
+
+    /**
+     * Returns the Date of the news.
+     */
+    public String getDate() {
+        return mDate;
+    }
+
+    /**
+     * Returns the Genre of the news.
+     */
+    public String getGenre() {
+        return mGenre;
+    }
+
+    /**
+     * Returns the website URL of the news.
+     */
+    public String getUrl() {
+        return mUrl;
+    }
+}
