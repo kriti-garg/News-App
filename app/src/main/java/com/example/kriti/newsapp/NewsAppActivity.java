7c9efc1eab317b32555eb353c0f60535f51d0473
Kriti Garg <kritigarg1998@gmail.com>
2018-04-08 00:04:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/kriti/newsapp/NewsAppActivity.java
@@ -0,0 +1,13 @@
+package com.example.kriti.newsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_app);
+    }
+}
